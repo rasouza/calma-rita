5adab97353e54ac3cd6a054cfb7d1025de5971e8
R. A. Souza <alves.wm@gmail.com>
2014-06-05 18:02:15 -0300

    Fixes mouse offset when drawing

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -14,10 +14,10 @@ $(function(){
 		canvas = $('#paper'),
 		ctx = canvas[0].getContext('2d'),
 		instructions = $('#instructions');
-	
+
 	// Generate an unique ID
 	var id = Math.round($.now()*Math.random());
-	
+
 	// A flag for drawing activity
 	var drawing = false;
 
@@ -25,46 +25,46 @@ $(function(){
 	var cursors = {};
 
 	var socket = io.connect(url);
-	
+
 	socket.on('moving', function (data) {
-		
+
 		if(! (data.id in clients)){
 			// a new user has come online. create a cursor for them
 			cursors[data.id] = $('<div class="cursor">').appendTo('#cursors');
 		}
-		
+
 		// Move the mouse pointer
 		cursors[data.id].css({
 			'left' : data.x,
 			'top' : data.y
 		});
-		
+
 		// Is the user drawing?
 		if(data.drawing && clients[data.id]){
-			
+
 			// Draw a line on the canvas. clients[data.id] holds
 			// the previous position of this user's mouse pointer
-			
+
 			drawLine(clients[data.id].x, clients[data.id].y, data.x, data.y);
 		}
-		
+
 		// Saving the current client state
 		clients[data.id] = data;
 		clients[data.id].updated = $.now();
 	});
 
 	var prev = {};
-	
+
 	canvas.on('mousedown',function(e){
 		e.preventDefault();
 		drawing = true;
-		prev.x = e.pageX;
-		prev.y = e.pageY;
-		
+		prev.x = e.pageX - canvas.offset().left;
+		prev.y = e.pageY - canvas.offset().top;
+
 		// Hide the instructions
 		instructions.fadeOut();
 	});
-	
+
 	doc.bind('mouseup mouseleave',function(){
 		drawing = false;
 	});
@@ -74,41 +74,42 @@ $(function(){
 	doc.on('mousemove',function(e){
 		if($.now() - lastEmit > 30){
 			socket.emit('mousemove',{
-				'x': e.pageX,
-				'y': e.pageY,
+				'x': e.pageX - canvas.offset().left,
+				'y': e.pageY - canvas.offset().top,
 				'drawing': drawing,
 				'id': id
 			});
 			lastEmit = $.now();
 		}
-		
+
 		// Draw a line for the current user's movement, as it is
 		// not received in the socket.on('moving') event above
-		
+
 		if(drawing){
-			
-			drawLine(prev.x, prev.y, e.pageX, e.pageY);
-			
-			prev.x = e.pageX;
-			prev.y = e.pageY;
+
+			drawLine(prev.x, prev.y, e.pageX - canvas.offset().left, e.pageY - canvas.offset().top);
+
+			prev.x = e.pageX - canvas.offset().left;
+			prev.y = e.pageY - canvas.offset().top;
+
 		}
 	});
 
 	// Remove inactive clients after 10 seconds of inactivity
 	setInterval(function(){
-		
+
 		for(ident in clients){
 			if($.now() - clients[ident].updated > 10000){
-				
-				// Last update was more than 10 seconds ago. 
+
+				// Last update was more than 10 seconds ago.
 				// This user has probably closed the page
-				
+
 				cursors[ident].remove();
 				delete clients[ident];
 				delete cursors[ident];
 			}
 		}
-		
+
 	},10000);
 
 	function drawLine(fromx, fromy, tox, toy){
